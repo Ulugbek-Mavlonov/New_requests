74f529b74975d6344a3f59053c5791d2345151c3
Ulugbek <ulugbekmavlonov@gmail.com>
2023-03-07 13:00:47 +0500

    addation new

--- requests_one.py
+++ requests_one.py
@@ -1,1 +1,8 @@
 import requests
+
+r = requests.get('https://reqres.in/api/users')
+data= r.json()
+for data in datas:
+    for key, value in data.items():
+        print(key, value)
+    print("\n")
